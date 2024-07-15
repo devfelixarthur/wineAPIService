3ba4117531a7a4de4290380e72881f6b5a75a03f
arthur_andrade_2024 <arthur.andrade@omnilink.com.br>
2024-07-15 18:43:31 -0300

    corrigindo cors

--- WineService/WineService/src/main/java/com/api/wiveService/WineService/config/security/SecurityConfiguration.java
+++ WineService/WineService/src/main/java/com/api/wiveService/WineService/config/security/SecurityConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,9 +22,7 @@ public class SecurityConfiguration {
     private SecurityFilter securityFilter;
 
     @Autowired
-
-    ResourcesExceptionHandler resourcesExceptionHandler;
-
+    private ResourcesExceptionHandler resourcesExceptionHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
