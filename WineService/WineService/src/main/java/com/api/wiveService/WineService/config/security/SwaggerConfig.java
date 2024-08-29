b149440d10d3ac2cd924da97b0e36c9d77be293a
Arthur Felix <arthur.andrade@omnilink.com.br>
2024-08-29 18:44:42 -0300

    Autenticação swagger

--- WineService/WineService/src/main/java/com/api/wiveService/WineService/config/security/SwaggerConfig.java
+++ WineService/WineService/src/main/java/com/api/wiveService/WineService/config/security/SwaggerConfig.java
@@ -19,12 +19,20 @@ import org.springframework.context.annotation.Configuration;
                 description = "WineService é uma API destinada a alimentar uma rede social de vinhos, " +
                         "oferecendo funcionalidades como cadastro de usuários, gerenciamento de vinhos (cadastro, edição e inativação), " +
                         "além de permitir a avaliação e comentários sobre os vinhos cadastrados. " +
-                        "Desenvolvida em Java 17, esta API utiliza Spring Boot e JPA, garantindo robustez e escalabilidade para o serviço.",
+                        "Desenvolvida em Java 17, esta API utiliza Spring Boot e JPA, garantindo robustez e escalabilidade para o serviço." +
+                        " " +
+                        "- Para conseguir as credencias entre em contrato (21) 97306-3532",
                 contact = @Contact(name = "Arthur Felix", email = "dev.felixarthur@gmail.com")
         ),
         externalDocs = @ExternalDocumentation(description = "Definições adicionais aqui")
 )
 
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class SwaggerConfig {
 
     @Bean
