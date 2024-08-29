fbe92a6138dea92ad04fdc8def095bcfb38c68fd
Arthur Felix <arthur.andrade@omnilink.com.br>
2024-08-29 18:20:50 -0300

    incluindo exemplos de email e senha

--- WineService/WineService/src/main/java/com/api/wiveService/WineService/domain/user/dto/AuthenticationDTO.java
+++ WineService/WineService/src/main/java/com/api/wiveService/WineService/domain/user/dto/AuthenticationDTO.java
@@ -1,14 +1,17 @@
 package com.api.wiveService.WineService.domain.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record AuthenticationDTO (
 
         @Email(message = "Forneeça um email válido.")
+        @Schema(description = "Email do usuário", example = "teste@swagger.com.br")
         String email,
 
         @NotBlank (message = "O campo password é obrigatório e não pode ser null")
+        @Schema(description = "Email do usuário", example = "teste@swagger")
         String password){
 
 }
